6f535e47d4b9011bbb39d49b64626c2ab483e061
rmbielski <rmbielski@gmail.com>
2025-07-30 14:56:32 -0600

    Removed all top-level non-directory files from repo and disk

--- check_training_affines.py
+++
@@ -1,9 +0,0 @@
-import nibabel as nib
-import glob
-import numpy as np
-
-affines = []
-for f in glob.glob('/mnt/beegfs/hellgate/home/rb194958e/Atlas_2/Training_Split/Masks/*.nii.gz'):
-    affines.append(nib.load(f).affine)
-affines = np.array(affines)
-print('Unique training mask affines:', len(np.unique(affines, axis=0)))
